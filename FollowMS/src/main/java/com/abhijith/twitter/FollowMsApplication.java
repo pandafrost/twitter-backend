df09d5242ff07fe442dfee68d9923c35f807bc05
Abhijithjakkam <abhijithjakkam@gmail.com>
2024-11-29 09:27:54 +0530

    first commit

---
+++ FollowMS/src/main/java/com/abhijith/twitter/FollowMsApplication.java
@@ -0,0 +1,13 @@
+package com.abhijith.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FollowMsApplication.class, args);
+	}
+
+}
---
+++ SearchMS/src/main/java/com/abhijith/twitter/SearchMsApplication.java
@@ -0,0 +1,13 @@
+package com.abhijith.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchMsApplication.class, args);
+	}
+
+}
